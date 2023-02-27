53aa788242056c9304421f4ec70ce5e8d1234af3
Chris Jenkins <cdpjenkins@gmail.com>
2023-02-27 19:58:08 +0000

    Print task stats per lecture 18

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,72 @@
-/***
- * Demo program to flash an LED attached to GPIO PAD 0.
- * Uses FreeRTOS Task
- * Jon Durrant
- * 15-Aug-2022
- */
-
+#include <stdexcept>
 
 #include "pico/stdlib.h"
 
 #include "FreeRTOS.h"
 #include "task.h"
-#include <stdio.h>
+#include <cstdio>
 
-
-//Standard Task priority
 #define TASK_PRIORITY		( tskIDLE_PRIORITY + 1UL )
 
-//LED PAD to use
 #define LED_PAD				0
 
-//Blink Delay
-#define DELAY				500
+// not that we're blinking anything at the moment
+#define BLINK_DELAY 500
 
 volatile int counter = 0;
 
+void task_stats() {
+    TaskStatus_t *pxTaskStatusArray;
+    volatile UBaseType_t uxArraySize, x;
+    unsigned long ulTotalRunTime;
+
+    uxArraySize = uxTaskGetNumberOfTasks();
+    printf("Number of tasks %lu\n", uxArraySize);
+
+    pxTaskStatusArray = (TaskStatus_t *)pvPortMalloc( uxArraySize * sizeof(TaskStatus_t) );
+
+    if(pxTaskStatusArray != nullptr) {
+        uxArraySize = uxTaskGetSystemState(pxTaskStatusArray,
+                                           uxArraySize,
+                                           &ulTotalRunTime);
+
+        for( x = 0; x < uxArraySize; x++ )
+        {
+            printf("Task: %lu \t cPri:%lu \t bPri:%lu \t hw:%4lu \t%s\n",
+                   pxTaskStatusArray[ x ].xTaskNumber ,
+                   pxTaskStatusArray[ x ].uxCurrentPriority ,
+                   pxTaskStatusArray[ x ].uxBasePriority ,
+                   pxTaskStatusArray[ x ].usStackHighWaterMark ,
+                   pxTaskStatusArray[ x ].pcTaskName
+            );
+        }
+
+        vPortFree(pxTaskStatusArray);
+    } else {
+        printf("Failed to allocate space for stats\n");
+        throw std::runtime_error("Failed to allocate space for stats");
+    }
+
+    HeapStats_t heapStats;
+    vPortGetHeapStats(&heapStats);
+    printf("HEAP avl: %d, blocks %d, alloc: %d, free: %d\n",
+           heapStats.xAvailableHeapSpaceInBytes,
+           heapStats.xNumberOfFreeBlocks,
+           heapStats.xNumberOfSuccessfulAllocations,
+           heapStats.xNumberOfSuccessfulFrees
+    );
+}
+
 [[noreturn]]
-void print_counter_value_main(void *params){
+void stats_main([[maybe_unused]] void *params) {
+    while (true) {
+        task_stats();
+        vTaskDelay(1000);
+    }
+}
+
+[[noreturn]]
+void print_counter_value_main([[maybe_unused]] void *params){
 	printf("print_counter_value_main task started\n");
 
 	const uint ledPad = LED_PAD;
@@ -33,21 +74,21 @@ void print_counter_value_main(void *params){
 
 	gpio_set_dir(ledPad, GPIO_OUT);
 
-    while (true) { // Loop forever
+    while (true) {
 //		gpio_put(ledPad, 1);
         printf("Counter is %d\n", counter);
-		vTaskDelay(DELAY);
+		vTaskDelay(BLINK_DELAY);
 //		gpio_put(ledPad, 0);
-//		vTaskDelay(DELAY);
+//		vTaskDelay(BLINK_DELAY);
 	}
 }
 
 [[noreturn]]
-void increment_counter_value_main(void *params){
+void increment_counter_value_main([[maybe_unused]] void *params){
     // Better not call printf in this thread because *I don't think it's threadsafe in FreeRTOS!*.
     while (true) { // Loop forever
         ++counter;
-        vTaskDelay(DELAY / 3);
+        vTaskDelay(BLINK_DELAY / 3);
     }
 }
 
@@ -67,6 +108,14 @@ void launch_tasks() {
                 nullptr,
                 TASK_PRIORITY,
                 &increment_counter_value_task);
+
+    TaskHandle_t stats_task;
+    xTaskCreate(stats_main,
+                "stats_task",
+                configMINIMAL_STACK_SIZE,
+                nullptr,
+                TASK_PRIORITY,
+                &stats_task);
 
     vTaskStartScheduler();
 }
