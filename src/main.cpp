41d72bb4ed48fdcf3d5b21d9164166c43e6296e8
Chris Jenkins <cdpjenkins@gmail.com>
2023-03-03 22:02:51 +0000

    Output tiny images on an attached Pico Unicorn

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,16 @@
 #include <stdexcept>
+#include <cstdio>
 
 #include "pico/stdlib.h"
+#include "pico_unicorn.hpp"
+using namespace pimoroni;
 
 #include "FreeRTOS.h"
 #include "task.h"
-#include <cstdio>
+
+#include "smile.h"
+#include "oi.h"
+#include "babi.h"
 
 #define TASK_PRIORITY		( tskIDLE_PRIORITY + 1UL )
 
@@ -14,6 +20,8 @@
 #define BLINK_DELAY 500
 
 volatile int counter = 0;
+
+PicoUnicorn pico_unicorn;
 
 void task_stats() {
     TaskStatus_t *pxTaskStatusArray;
@@ -92,6 +100,45 @@ void increment_counter_value_main([[maybe_unused]] void *params){
     }
 }
 
+[[noreturn]]
+void leds_main([[maybe_unused]] void *params){
+
+    const int WIDTH = 16;
+    const int HEIGHT = 7;
+
+
+    pico_unicorn.init();
+
+    while (true) { // Loop forever
+
+        pico_unicorn.clear();
+        printf("About to say oi...\n");
+        {
+            int i = 0;
+            for (int y = 0; y < HEIGHT; y++) {
+                for (int x = 0; x < WIDTH; x++) {
+                    pico_unicorn.set_pixel(x, y, oi[i++], oi[i++], oi[i++]);
+                }
+            }
+        }
+        printf("said oi\n");
+        sleep_ms(1000);
+
+        pico_unicorn.clear();
+        printf("About to say babi\n");
+        {
+            int i = 0;
+            for (int y = 0; y < HEIGHT; y++) {
+                for (int x = 0; x < WIDTH; x++) {
+                    pico_unicorn.set_pixel(x, y, babi[i++], babi[i++], babi[i++]);
+                }
+            }
+        }
+        printf("said babi\n");
+        sleep_ms(1000);
+    }
+}
+
 void launch_tasks() {
     TaskHandle_t print_counter_value_task;
     xTaskCreate(print_counter_value_main,
@@ -116,6 +163,14 @@ void launch_tasks() {
                 nullptr,
                 TASK_PRIORITY,
                 &stats_task);
+
+    TaskHandle_t leds_task;
+    xTaskCreate(leds_main,
+                "leds_task",
+                configMINIMAL_STACK_SIZE,
+                nullptr,
+                TASK_PRIORITY,
+                &leds_task);
 
     vTaskStartScheduler();
 }
